9f1ad39ba93e185ce3326bb5aa28dba42d12adb1
Badi <p.soenarto@web.de>
2018-05-04 02:05:01 +0200

    Panzer bewegen mit Pfeiltasten

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,11 +7,15 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D Char; 
     public float speed = 100;
     public float gravity = -0.5f;
-    //public Rigidbody2D rb;
+    public Rigidbody2D rb;
+    private Animator an;
 
-    
+
     public bool is_movable = false;
     private SpriteRenderer m_FacingRight;
+
+    public Transform bodyTransform;
+    public float velocity;
 
 
     void Start()
@@ -71,6 +75,38 @@ public class PlayerMovement : MonoBehaviour {
             Debug.Log(this.is_movable + "für" + this.gameObject);
         }
                this.is_movable = true;
+    }
+    void Move()
+    {
+        GameObject[] objs;
+        objs = GameObject.FindGameObjectsWithTag("Tank");
+
+        foreach (GameObject tank in objs)
+        {
+            if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.velocity = Vector2.right * velocity;
+                if (bodyTransform.localScale.x > 0f)
+                    bodyTransform.localScale = new Vector3(-bodyTransform.localScale.x, bodyTransform.localScale.y, 0f);
+
+                an.SetBool("moving", true);
             }
+            else if (!Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.velocity = -Vector2.right * velocity;
+                if (bodyTransform.localScale.x < 0f)
+                    bodyTransform.localScale = new Vector3(-bodyTransform.localScale.x, bodyTransform.localScale.y, 0f);
+
+                an.SetBool("moving", true);
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+                an.SetBool("moving", false);
+            }
+        }
+        this.is_movable = true;
+    }
+
 }
 
