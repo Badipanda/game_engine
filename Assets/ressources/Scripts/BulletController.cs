0416a816abcc7294c5e20e4d8cd5bcc13465245e
JiBiT(Marcel) <mjibit@web.de>
2018-06-04 18:44:17 +0200

    init mit ignore

---
+++ Assets/ressources/Scripts/BulletController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+	public CircleCollider2D destructionCircle;
+	public Vector2 mousePosition ;
+	public Vector2 firePointPosition ;
+	public static GroundController groundController;
+
+	// Use this for initialization
+	protected virtual void  Start ()
+	{
+		gameObject.GetComponent<Rigidbody2D>().AddForce((mousePosition - firePointPosition) * 100);
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+       
+	}
+
+	void OnCollisionEnter2D (Collision2D coll)
+	{
+		Debug.Log ("enter collision");
+
+		if (coll.collider.tag == "Ground") {
+
+			groundController.DestroyGround (destructionCircle);
+			Destroy (gameObject);
+            
+		} else if (coll.collider.tag == "Tank" || coll.collider.tag == "Hitable") {
+//			Destroy (gameObject);
+			print ("hit Tank");
+		}
+	}
+
+	void OnBecameInvisible ()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/ressources/Scripts/Splittergranate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splittergranate : Bullet{
+    
+
+
+
+
+	public Splittergranate(GameObject bulletType, float bulletSpeed, Transform bulletPosition) : base(bulletType, bulletSpeed, bulletPosition)
+    {
+		
+    }
+//
+//	public void Reset(){
+//		
+//	}
+
+    
+
+	public override void shoot()
+	{
+		Debug.Log("shoot2");
+
+
+		Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+		Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+
+		RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100);
+		Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.green);
+
+		GameObject Clone;
+
+		Clone = (GameObject.Instantiate(bullet, firePoint.position, firePoint.rotation)) as GameObject;
+
+		Debug.Log("BuLLet erstellt über Splitter");
+
+
+		Clone.GetComponent<Rigidbody2D>().AddForce((mousePosition - firePointPosition) * speed);
+
+
+
+
+	}
+
+   
+
+    
+}
