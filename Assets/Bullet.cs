718b441f0133a9d2e2c6d98d2672e2b0c531dc1a
Badi <p.soenarto@web.de>
2018-06-04 21:22:34 +0200

    stand von samstag 02-06.18 wiederhergestellt

---
+++ Assets/Bullet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet  {
+    public GameObject bullet;
+    public float speed;
+    public Transform firePoint;
+
+
+
+    public Bullet(GameObject bulletType, float bulletSpeed, Transform bulletPosition)
+    {
+        bullet = bulletType;
+        speed = bulletSpeed;
+        firePoint = bulletPosition;
+    }
+
+    void Update()
+    {
+        checkCollision();
+
+    }
+
+    public void shoot()
+    {
+        Debug.Log("shoot");
+
+
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100);
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.green);
+
+        GameObject Clone;
+        Clone = (GameObject.Instantiate(bullet, firePoint.position, firePoint.rotation)) as GameObject;
+        Debug.Log("BuLLet erstellt");
+
+
+        Clone.GetComponent<Rigidbody2D>().AddForce((mousePosition - firePointPosition) * speed);
+
+
+
+    }
+
+    public void checkCollision()
+    {
+        Debug.Log("checkCollision");
+    }
+
+    public void explode()
+    {
+        Debug.Log("checkCollision");
+    }
+
+    
+}
---
+++ Assets/SplittergranateBehaviour.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplittergranateBehaviour : MonoBehaviour
+{
+    public int splitter_count = 10;
+    public GameObject[] gos;
+    public static GroundController groundController;
+    public bool isCreated = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            fire();
+        }
+
+    }
+
+    void fire()
+    {
+        //Clone of the bullet
+        if (!isCreated)
+        {
+            float bla = 1f;
+            //GameObject Clone;
+            //Clone = (Instantiate(gameObject, transform.position, transform.rotation)) as GameObject;
+            gos = new GameObject[splitter_count];
+            for (int i = 0; i < gos.Length; i++)
+            {
+                bla = bla *(-1f);
+                GameObject clone = (GameObject)Instantiate(gameObject, transform.position, transform.rotation);
+                clone.GetComponent<Rigidbody2D>().AddForce(new Vector2((20*bla),(10 * bla))*10f);
+                gos[i] = clone;
+            }
+
+
+            //Instantiate(gameObject, transform.position, transform.rotation);
+            //Instantiate(gameObject, transform.position, transform.rotation);
+            //Instantiate(gameObject, transform.position, transform.rotation);
+
+
+            GameObject.Destroy(gameObject);
+            isCreated = true;
+
+
+
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+      
+        Debug.Log("Collision Detection");
+        if (other.gameObject.CompareTag("Tank"))
+        {
+           
+            Debug.Log("is HITABLE");
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
